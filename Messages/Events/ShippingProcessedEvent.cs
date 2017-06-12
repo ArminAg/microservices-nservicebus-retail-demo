d4764849e8838aa048e321a8311ea7ffbba08a28
ArminAg <armin.ag@outlook.com>
2017-06-13 00:36:51 +0100

    Add ShippingProcessedEvent to Messages

---
+++ Messages/Events/ShippingProcessedEvent.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages.Events
+{
+    public class ShippingProcessedEvent : IEvent
+    {
+        public string OrderId { get; set; }
+    }
+}
