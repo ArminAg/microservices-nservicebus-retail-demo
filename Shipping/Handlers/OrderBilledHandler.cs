3d3f7bc1b1441d9b6928ca7e975d01493e53079d
ArminAg <armin.ag@outlook.com>
2017-06-12 22:45:19 +0100

    Add Handler for OrderBilledEvent in Shipping

---
+++ Shipping/Handlers/OrderBilledHandler.cs
@@ -0,0 +1,22 @@
+﻿using Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Handlers
+{
+    public class OrderBilledHandler : IHandleMessages<OrderBilledEvent>
+    {
+        static ILog logger = LogManager.GetLogger<OrderBilledHandler>();
+
+        public Task Handle(OrderBilledEvent message, IMessageHandlerContext context)
+        {
+            logger.Info($"Received OrderBilledEvent, OrderId = { message.OrderId } - Shipping order...");
+            return Task.CompletedTask;
+        }
+    }
+}
