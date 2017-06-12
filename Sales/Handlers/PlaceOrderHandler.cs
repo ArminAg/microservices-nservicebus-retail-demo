d4bd4a42c1ab7ad9843a59d60dd53aa162165c4e
ArminAg <armin.ag@outlook.com>
2017-06-12 17:33:45 +0100

    Move PlaceOrderHandler to Sales

--- Sales/Handlers/PlaceOrderHandler.cs
+++ Sales/Handlers/PlaceOrderHandler.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClientUI.Handlers
+namespace Sales.Handlers
 {
     public class PlaceOrderHandler : IHandleMessages<PlaceOrderCommand>
     {
