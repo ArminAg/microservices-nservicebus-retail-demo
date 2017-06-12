727d1636581b8202313199f20907233f3bd1daf9
ArminAg <armin.ag@outlook.com>
2017-06-12 22:52:47 +0100

    Add OrderPlacedHandler in Shipping

---
+++ Shipping/Handlers/OrderPlacedHandler.cs
@@ -0,0 +1,22 @@
+﻿using Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Handlers
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlacedEvent>
+    {
+        static ILog logger = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlacedEvent message, IMessageHandlerContext context)
+        {
+            logger.Info($"Received OrderPlacedEvent, OrderId = { message.OrderId } - Should we ship now?");
+            return Task.CompletedTask;
+        }
+    }
+}
