6e014b58cdb8c12f8f5ff22c6845d0d38e53c9c4
ArminAg <armin.ag@outlook.com>
2017-06-12 15:53:56 +0100

    Configure an Endpoint for ClientUI

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace ClientUI
 
         static async Task AsyncMain()
         {
+            Console.Title = "ClientUI";
+
+            // EndpointConfiguration class is where we define all the settings that determine how our endpoint will operate
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            // This setting defines the transport that NServiceBus will use to send and receive messages
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+            // When sending messages, an endpoint needs to serialize message objects to a stream, and then deserialize the stream back to a message object on the receiving end
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            // A persistence is required to store some data in between handling messages
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+
+            await endpointInstance.Stop().ConfigureAwait(false);
 
         }
     }
