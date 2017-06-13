cad849efc6d0a3e63d90c9366b2d6c2ae0ca5492
ArminAg <armin.ag@outlook.com>
2017-06-13 15:01:25 +0100

    Create Endpoint for Planning Service

--- Planning/Global.asax.cs
+++ Planning/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Messages.Events;
+using NServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +23,39 @@ namespace Planning
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ConfigureEndpoint();
+        }
+
+        private void ConfigureEndpoint()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            var endpointConfiguration = new EndpointConfiguration("Planning");
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.UseContainer<AutofacBuilder>(
+                customizations =>
+                {
+                    customizations.ExistingLifetimeScope(container);
+                });
+
+            var endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            var updater = new ContainerBuilder();
+            updater.RegisterInstance(endpoint);
+            updater.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var updated = updater.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(updated);
         }
     }
 }
