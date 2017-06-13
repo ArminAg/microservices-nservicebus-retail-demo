dab27bf1068cce35b3d1e1395173a9440bf95ac9
ArminAg <armin.ag@outlook.com>
2017-06-13 09:23:01 +0100

    Configure WebApi Endpoint

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using NServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +22,36 @@ namespace WebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ConfigureEndpoint();
+        }
+
+        private void ConfigureEndpoint()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            var endpointConfiguration = new EndpointConfiguration("WebApi");
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.UseTransport<MsmqTransport>();
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.UseContainer<AutofacBuilder>(
+                customizations =>
+                {
+                    customizations.ExistingLifetimeScope(container);
+                });
+
+            var endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            var updater = new ContainerBuilder();
+            updater.RegisterInstance(endpoint);
+            updater.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var updated = updater.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(updated);
         }
     }
 }
