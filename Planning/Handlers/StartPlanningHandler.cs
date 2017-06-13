589d612ee436216077de4b34b830a653513585ac
ArminAg <armin.ag@outlook.com>
2017-06-13 15:38:19 +0100

    Add Handler for StartPlanningCommand in Planning Microservice

---
+++ Planning/Handlers/StartPlanningHandler.cs
@@ -0,0 +1,22 @@
+﻿using Messages.Commands;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+
+namespace Planning.Handlers
+{
+    public class StartPlanningHandler : IHandleMessages<StartPlanningCommand>
+    {
+        static ILog logger = LogManager.GetLogger<StartPlanningHandler>();
+
+        public Task Handle(StartPlanningCommand message, IMessageHandlerContext context)
+        {
+            logger.Info($"Received StartPlanningCommand, PlanId = { message.PlanId }, Planning Started");
+            return Task.CompletedTask;
+        }
+    }
+}
