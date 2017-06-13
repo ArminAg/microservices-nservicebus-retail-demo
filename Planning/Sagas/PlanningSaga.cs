953bd724e27f890fa146bbb04767094b154bba85
ArminAg <armin.ag@outlook.com>
2017-06-13 16:56:49 +0100

    Create saga to Start planning when to order articles that are low on stock

---
+++ Planning/Sagas/PlanningSaga.cs
@@ -0,0 +1,30 @@
+﻿using Messages.Commands;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+
+namespace Planning.Sagas
+{
+    public class PlanningSaga : Saga<PlanningSagaData>,
+        IAmStartedByMessages<StartPlanningCommand>
+
+    {
+        static ILog logger = LogManager.GetLogger<PlanningSaga>();
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<PlanningSagaData> mapper)
+        {
+            mapper.ConfigureMapping<StartPlanningCommand>(message => message.PlanId)
+                .ToSaga(sagaData => sagaData.PlanId);
+        }
+
+        public Task Handle(StartPlanningCommand message, IMessageHandlerContext context)
+        {
+            logger.Info($"Planning is started for PlanId = { message.PlanId }");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Planning/Sagas/PlanningSagaData.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Planning.Sagas
+{
+    public class PlanningSagaData : ContainSagaData
+    {
+        public string PlanId { get; set; }
+    }
+}
