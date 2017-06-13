1d40d1703a85e57965c28270383f98160bee5574
ArminAg <armin.ag@outlook.com>
2017-06-13 09:30:11 +0100

    Add WebApi Handler for ShippingProcessedEvent

---
+++ WebApi/Handlers/ShippingProcessedHandler.cs
@@ -0,0 +1,22 @@
+﻿using Messages.Events;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+
+namespace WebApi.Handlers
+{
+    public class ShippingProcessedHandler : IHandleMessages<ShippingProcessedEvent>
+    {
+        static ILog logger = LogManager.GetLogger<ShippingProcessedHandler>();
+
+        public Task Handle(ShippingProcessedEvent message, IMessageHandlerContext context)
+        {
+            logger.Info($"Received ShippingProcessedEvent, OrderId = { message.OrderId }, Doing some additional operations...");
+            return Task.CompletedTask;
+        }
+    }
+}
