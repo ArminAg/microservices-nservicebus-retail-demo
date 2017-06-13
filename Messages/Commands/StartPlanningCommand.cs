b74a5dc1c7e60b1233c69dc12ffdc7462a246c12
ArminAg <armin.ag@outlook.com>
2017-06-13 15:31:59 +0100

    Create StartPlanningCommand and send it to Planning service

---
+++ Messages/Commands/StartPlanningCommand.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages.Commands
+{
+    public class StartPlanningCommand : ICommand
+    {
+        public string PlanId { get; set; }
+        public string PlanName { get; set; }
+    }
+}
---
+++ WebApi/Controllers/PlannerController.cs
@@ -0,0 +1,54 @@
+﻿using Messages.Commands;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class PlannerController : ApiController
+    {
+        private IEndpointInstance endpointInstance;
+
+        public PlannerController(IEndpointInstance endpointInstance)
+        {
+            this.endpointInstance = endpointInstance;
+        }
+
+        // GET: api/Planner
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Planner/5
+        public async Task<string> Get(int id)
+        {
+            await endpointInstance.Send("Planning", new StartPlanningCommand
+            {
+                PlanId = Guid.NewGuid().ToString(),
+                PlanName = "Test Plan"
+            });
+            return "value";
+        }
+
+        // POST: api/Planner
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Planner/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Planner/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
